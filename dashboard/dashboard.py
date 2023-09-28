cfb0ae63f0033ab4796fb25b91bf7369219e97dd
Axelino Natanael Ndahawali <99704941+axelnatan@users.noreply.github.com>
2023-09-28 12:24:07 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -10,7 +10,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     workingday = data.groupby(by="workingday")["cnt"].sum()
-    st.bar_chart(x=workingday.index,y=workingday.values)
+    st.bar_chart(workingday,x=workingday.index,y=workingday.values)
 with col2:
     weathersit = data.groupby(by="weathersit")["cnt"].sum()
-    st.bar_chart(x=weathersit.index,y=weathersit.values)
+    st.bar_chart(weathersit,x=weathersit.index,y=weathersit.values)
